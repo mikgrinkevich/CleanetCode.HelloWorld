81184347c85270910fed5e3edc62e3e39e0ccabc
mikgrinkevich <85791543+mikgrinkevich@users.noreply.github.com>
2022-02-08 14:32:19 +0300

    description updated

--- CleanetCode.HelloWorld.CLI/Program.cs
+++ CleanetCode.HelloWorld.CLI/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Why to become a programmer?\nTo understand how things around work and" +
+    "make my own life better and life of others better" +
+    "To think, to solve problems and to create");
